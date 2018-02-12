5c8c0c4a11de1685f39dcb7738a2728dc1398654
sjw0484 <sjw0484@naver.com>
2018-02-12 21:18:34 +0900

    코드아카데미 과정 기록

---
+++ js-tutorial.js
@@ -0,0 +1,10 @@
+console.log("Teaching the world how to code".length);
+//글자 수 측정
+
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase()); 
+//대문자
+
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim()); 
+//공백 제거
