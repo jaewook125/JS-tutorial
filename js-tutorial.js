511b4a764efd8a09566ad15422084fbccaa262ef
sjw0484 <sjw0484@naver.com>
2018-02-13 23:49:52 +0900

    js tutorial 2

--- js-tutorial.js
+++ js-tutorial.js
@@ -8,3 +8,13 @@ console.log('Codecademy'.toUpperCase());
 // Use a string method to log the following statment without whitespace at the beginning and end of it.
 console.log('    Remove whitespace   '.trim()); 
 //공백 제거
+
+
+console.log(Math.random()*100);
+// 16진수 숫자 1~100
+console.log(Math.floor(Math.random()*100));
+// 소수점 제거
+console.log(Math.ceil(43.8)*100);
+// 43.8 이하 호출하지않음
+console.log(Number.isInteger(2017));
+// 정수인지 확인함
