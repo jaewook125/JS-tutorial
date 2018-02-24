e92e52f26fe4635a1f92152b30ee3d7f2b7bd137
sjw0484 <sjw0484@naver.com>
2018-02-24 14:48:17 +0900

    web_js add

---
+++ web_js.js
@@ -0,0 +1,37 @@
+//길이측정
+"yourname".length
+
+//add
+3+4
+
+//팝업창 자바스크립트
+confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
+
+//팝업창 프롬프트 대화형식
+prompt("question!");
+
+//띄어쓰기도 숫자에 포함
+"cake is good".length
+
+//문자열 booleans측정 가능
+"I'm coding like a champ".length > 10
+
+//콘솔 띄우기 가능
+console.log(2*5)
+console.log("Hello")
+
+//간단한 제어문
+if ("myname".length >= 6 ) {
+    console.log("zz");
+}
+
+//거짓일경우
+if (false) 
+{
+    console.log("Let's go down the first road!");
+}
+else 
+{
+    // What should we do if the condition is false? Fill in here:
+    console.log("not good");
+}
