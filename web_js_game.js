110082b4d1ded3f3ea702f9774d45dc06f8e8397
sjw0484 <sjw0484@naver.com>
2018-02-24 15:33:48 +0900

    js_game add

---
+++ web_js_game.js
@@ -0,0 +1,12 @@
+//팝업창 띄우기
+confirm("I am ready to play!");
+
+//나이묻는 게임
+var age = prompt("What's your age");
+//프롬프트에 입력된 숫자를 age변수에 저장후
+if (age >= 13){
+	//대입
+    console.log("13 up");    
+} else {
+    console.log("13 down");    
+}
--- web_js_var.js
+++ web_js_var.js
@@ -2,3 +2,9 @@
 var myAge = 26;
 
 console.log(myAge);
+
+//변수 상속
+var myName = "jae";
+console.log(myName);
+myName = "jae";
+console.log(myName.substring(0,2));//ja 출력
