fe64b2bba37878df3f57fb9122a921a4aedb848a
sjw0484 <sjw0484@naver.com>
2018-02-26 11:34:35 +0900

    for 루프 추가

---
+++ web_js_for.js
@@ -0,0 +1,24 @@
+//1~10 루프
+for (var counter = 1; counter < 11; counter++) {
+	console.log(counter);
+}
+
+//5로 시작 5식 더해서 50까지
+for (var i = 5; i < 51; i += 5) {
+	console.log(i);
+}
+
+//8로시작 12식 더해져서 120까지
+for (var i = 8 ; i < 120; i+=12) {
+	console.log(i);
+}
+
+//10으로 시작 1식 감소해서 0까지
+for (var i = 10; i >= 0; i--) {
+	console.log(i);
+}
+
+//100으로 시작 5식 감소해서 1까지
+for (var i = 100; i > 0 ; i -= 5){
+    console.log(i)
+}
